ed34e83f957e4bc747a5af87ba8520f6d933ffe9
Carlosh2706 <carlosh2706@hotmail.com>
2020-11-11 15:20:52 -0600

    Create shifts added

---
+++ lib/screens/home/widgets/createShifts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OurCreateShifts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Text("Create Shifts", textScaleFactor: 2.0,)
+    );
+  }
+}
